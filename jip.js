8c6f9d12a74de74badad07278d26a1c7bcbe5824
zaidkhan1506 <zk972631@gmail.com>
2023-07-11 13:19:39 +0530

    added name

--- jip.js
+++ jip.js
@@ -1,7 +1,11 @@
 const myName = "Bard";
+const your = "zaid";
 
 const sayHello = () => {
   console.log(`Hello, ${myName}`);
+
+const sayHello = () => {
+  console.log(`Hello, ${your}`);
 };
 
 sayHello();
